c10d1d7acde74817e54373dc9b3fc7d37f8908c9
bkcha8 <bkcha8@student.monash.edu>
2015-10-21 22:40:50 +1100

    Create function.js
    Function for da stopwatch

---
+++ function.js
@@ -0,0 +1,34 @@
+var c=-1
+var m=0,h=0;
+var timer=0;
+function a()
+    {
+       timedCount();
+     //   tracking()
+        
+    }
+function timedCount()
+ {
+
+ c=c+1;
+    if(c<60) {document.getElementById('timer').innerHTML=h+"h:"+m+"min:"+c+"s"}
+    else if (c>60){ c=c%60;m=m+1;document.getElementById('timer').innerHTML=h+"h:"+m+"min:"+c+"s"};
+     
+    if(m<60){ document.getElementById('timer').innerHTML=h+"h:"+m+"min:"+c+"s"}
+    else if(m>60){h=h+1;m=m%60}
+
+ t=setTimeout("timedCount()",1000)
+ }
+    
+function stopCount()
+ {
+ clearTimeout(t);
+     time=c;
+ }
+
+function resetCount()
+{
+    c=m=h=0;
+    document.getElementById('timer').innerHTML=h+"h:"+m+"min:"+c+"s";
+    
+}
